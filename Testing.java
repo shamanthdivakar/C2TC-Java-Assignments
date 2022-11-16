a65aafff0068880a37e475b48d559a2c9f8e3184
shamanth D <92384553+shamanthdivakar@users.noreply.github.com>
2022-11-16 20:09:00 +0530

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,25 @@
+package projectjunit5;
+import org.junit.jupiter.api.Test;
+public class Testing {
+
+	public class fp1 {
+	//	@Disabled
+		void disp()
+		{
+			System.out.println("hi everyone");
+			
+		}
+		@Test
+		void disp2()
+		{
+			System.out.println("HELLO");
+		}
+		@Test
+		void disp3()
+		{
+			System.out.println("HELLO");
+		}
+		
+	}
+
+}
